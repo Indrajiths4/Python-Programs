f3d56a7881c0d855423c38c06f8c45bf5825cf8b
Indrajith S Nair <95161630+Indrajiths4@users.noreply.github.com>
2024-03-22 00:35:17 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,25 @@
+# my_dict = {'apple': 5, 'banana': 3, 'orange': 8, 'grapes': 2}
+# sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1]))
+# print("Sorted Dictionary by Values:", sorted_dict)
+
+
+# text = "This is a sample text . This text contains some words. Sample text is for demonstration."
+# text = text.lower()
+# words = text.split()
+# word_freq = {}
+# for word in words:
+    
+#     if word in word_freq:
+#         word_freq[word] += 1
+    
+#     else:
+#         word_freq[word] = 1
+# for word, freq in word_freq.items():
+#     print(f"{word}: {freq}")
+
+
+# dict1 = {'a': 1, 'b': 2}
+# dict2 = {'c': 3, 'd': 4}
+
+# merged_dict = {**dict1, **dict2}
+# print("Merged Dictionary:", merged_dict)
