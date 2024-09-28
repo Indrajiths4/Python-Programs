ef7668edc0252979e45d0bb60e88deeee968d705
Indrajith S Nair <indrajithmundackal@gmail.com>
2024-09-28 18:52:38 +0530

    Update grammercorrector.py

--- GrammerCorrector/grammercorrector.py
+++ GrammerCorrector/grammercorrector.py
@@ -1,13 +1,16 @@
-import spacy
 import language_tool_python
-nlp = spacy.load("en_core_web_sm")
+
 tool = language_tool_python.LanguageTool('en-US')
+
 def correct_sentence(sentence):
     matches = tool.check(sentence)
     corrected_sentence = tool.correct(sentence)
     return corrected_sentence, matches
-input_sentence = "I is going to india."
+
+input_sentence = "She go to the market yesterday."
+
 corrected_sentence, errors = correct_sentence(input_sentence)
+
 print(f"Original Sentence: {input_sentence}")
 print(f"Corrected Sentence: {corrected_sentence}")
 print(f"Errors Found: {len(errors)}")
